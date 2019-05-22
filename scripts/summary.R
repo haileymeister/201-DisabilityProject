81624ad0e65e6a3d201f89640649bbfbad5d2933
Sophie Hurst <sophiehurst@Sophies-MacBook-Pro.local>
2019-05-22 16:36:59 -0700

    Fix lintr errors

--- scripts/summary.R
+++ scripts/summary.R
@@ -1,25 +1,24 @@
 # Load packages
 library(dplyr)
+library(lintr)
 
 # Create a function to gather summary information from a dataset
 summary_info <- function(dataset) {
   summary <- list()
   summary$length <- length(dataset)
   summary$height <- nrow(dataset)
-  summary$race <- dataset %>% 
-    group_by(RaceCode) %>% 
-    summarise(total_per_race = n()) %>% 
+  summary$race <- dataset %>%
+    group_by(RaceCode) %>%
+    summarise(total_per_race = n()) %>%
     pull(RaceCode)
-  summary$locations <- dataset %>% 
-    group_by(GeographicLocation) %>% 
-    summarise(total_per_location = n()) %>% 
+  summary$locations <- dataset %>%
+    group_by(GeographicLocation) %>%
+    summarise(total_per_location = n()) %>%
     pull(GeographicLocation)
-  summary$disability <- dataset %>% 
-    group_by(DisabilityStatus) %>% 
-    summarise(total_disability = n()) %>% 
-    filter(total_disability == max(total_disability)) %>% 
+  summary$disability <- dataset %>%
+    group_by(DisabilityStatus) %>%
+    summarise(total_disability = n()) %>%
+    filter(total_disability == max(total_disability)) %>%
     pull(DisabilityStatus)
-  return (summary)
+  return(summary)
 }
-
-
