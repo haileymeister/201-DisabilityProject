704579834982e54570d0d3e5f6b709304d3bf62a
Emily Fanjia Kong <emkong@uw.edu>
2019-05-23 16:31:27 -0700

    updated stacked bar

--- summary.R
+++
@@ -1,7 +0,0 @@
-# Load the `dplyr` package
-library("dplyr") 
-
-# Load data
-data <- read.csv("data/aging-and-disability-services-client-level-data-2016.csv", stringsAsFactors = FALSE)
-
-View(data)
