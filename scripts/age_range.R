de92c9649725753e8d240dc1652bc9f6074fb49b
Emily Fanjia Kong <emkong@uw.edu>
2019-05-24 11:02:55 -0700

    updated stacked bar

--- scripts/age_range.R
+++ scripts/age_range.R
@@ -1,5 +1,5 @@
 # Load the `dplyr` package
-library("dplyr") 
+library("dplyr")
 library("tidyverse")
 library("ggplot2")
 library("plotly")
@@ -17,16 +17,17 @@ stacked_bar_chart <- function(data) {
     summarize(LiveAlone = n(), DisabilityStatus = n(),
               NutritionalRisk = n()) %>%
     gather(key = LivingStatus, value = NumPeople, -AgeRange)
-  
+
   age_plot <-
-    ggplot(data_age, mapping = aes(x = AgeRange, y = NumPeople, fill = LivingStatus)) + 
+    ggplot(data_age, mapping = aes(x = AgeRange,
+                                   y = NumPeople, fill = LivingStatus)) +
     geom_bar(stat = "identity") +
     labs(title = "Number of People with Disadvantages Based on Age",
          x = "Age Range",
          y = "Number of People",
          fill = "Disadvantage Status"
     ) + theme(axis.text.x = element_text(angle = 30))
-  
+
   plot <- ggplotly(age_plot)
   return(plot)
 }
