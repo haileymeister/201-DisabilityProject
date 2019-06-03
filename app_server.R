4afa79ebc57e76857a9a8fffb88c811acd478c58
Emily Fanjia Kong <emkong@uw.edu>
2019-06-03 01:28:29 -0700

    pulling

--- app_server.R
+++ app_server.R
@@ -1,6 +1,12 @@
 # Load packages
 library(shiny)
 
+source("./scripts/age_range.R")
+
 server <- function(input, output) {
+  output$neighborhood <- renderPlotly({
+    return(bar_chart(cleaned, input$fill_input, input$year_input))
+  })
   
+  output$
 }
