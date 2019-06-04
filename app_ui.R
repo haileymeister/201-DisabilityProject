60e565ea0b6730d5ce0fa79313ec58b44964cafc
Katherine Brower <kbrower@uw.edu>
2019-06-04 14:36:40 -0700

    edit

--- app_ui.R
+++ app_ui.R
@@ -127,8 +127,30 @@ page_age <- tabPanel(
   )
 )
 
-page_three <- tabPanel(
-  "page three"
+scatter_page <- tabPanel(
+  "Demographics of Seattle through 2010 to 2016",
+  disability_by_year,
+  demo_sidebar
+)
+
+#Main panel
+disability_by_year <- mainPanel(
+  plotlyOutput("demo_linegraph")
+)
+
+#Sidebar panel Widget
+demo_sidebar <- sidebarPanel(
+  variable_input <- selectInput(
+    "variable_input",
+    label = "Demographic Variable of Interest",
+    choices = list("Disability Status" = "DisabilityStatus",
+                   "Veteran Status" = "Veteran",
+                   "Homeless" = "Homeless",
+                   "Driving Impairment" = "Driving",
+                   "Limited English"= "LimitedEnglish",
+                   "Households with Children" = "HouseholdWithChildren"),
+    selected = list("Disability Status" = "DisabilityStatus")
+  )
 )
 
 sum_page <- tabPanel(
