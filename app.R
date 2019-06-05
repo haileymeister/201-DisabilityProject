013b642c1bb516af60c6ad2ab737a02a04df985a
Hailey Meister <hmeister@uw.edu>
2019-06-04 21:15:52 -0700

    change

--- app.R
+++ app.R
@@ -7,7 +7,3 @@ source("app_server.R")
 
 # Create a new shinyApp() using the sourced ui and server
 shinyApp(ui = ui, server = server)
-
-
-#rsconnect::setAccountInfo(name='haileymeister', token='EBB992396D871AEADD891E368C0E807B', secret='A6wXijndZaODG/aiUxnT6CvkYUo4cr8z4wjoRWH1')
-#deployApp()
